34338fd1bf9ce55aa65353b6ce07ff2c6021c063
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-28 23:20:26 +0530

    Added Changes

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ app.secret_key = "visa"
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 jwt = JWTManager(app)
@@ -54,6 +54,6 @@ api.add_resource(History, "/transaction/history")
 
 
 if __name__ == "__main__":
-    db.init_app(app)
+    # db.init_app(app)
     ma.init_app(app)
     app.run(port=5002, debug=True)
