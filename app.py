208c0dcc844955712e1f8ce3a57f6876f17e29ac
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-28 23:06:57 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from marshmallow import ValidationError
+
+from ma import ma
+from db import db
+from blacklist import BLACKLIST
+
+from resource.merchant import (
+    MerchantRegister, 
+    MerchantLogin, 
+    MerchantLogout, 
+    Merchant, 
+    TokenRefresh, 
+    ReceivePayment
+)
+
+from resource.history import History
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True  # enable blacklist feature
+app.config["JWT_BLACKLIST_TOKEN_CHECK"] = [
+    "access",
+    "refresh"
+]  # allowing blacklisting for access and refresh tokens
+app.secret_key = "visa"
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWTManager(app)
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token["jti"] in BLACKLIST
+
+
+api.add_resource(MerchantRegister, "/register")
+api.add_resource(MerchantLogin, "/login")
+api.add_resource(MerchantLogout, "/logout")
+api.add_resource(Merchant, "/merchant/<string:mobile_number>")
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(ReceivePayment, "/payment")
+api.add_resource(History, "/transaction/history")
+
+
+if __name__ == "__main__":
+    db.init_app(app)
+    ma.init_app(app)
+    app.run(port=5002, debug=True)
---
+++ authApi/visaNet.py
@@ -0,0 +1,33 @@
+import requests
+
+
+headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+timeout = 20
+
+
+class VisaNet:
+    @classmethod
+    def AmountConfirmation(cls,payload):
+        print("AmountConfirmation")
+        url = "https://virtual-card-auth.herokuapp.com/visa_net/payment"
+        # url = "http://127.0.0.1:5001/visa_net/payment"
+        try:
+            r= requests.post(url,json=payload,headers=headers, timeout=timeout)
+            return r
+        except Exception as e:
+            print(e)
+            return {"msg": INTERNAL_SERVER_ERROR}, 500
+        
+    @classmethod
+    def TransactionConfirmation(cls, payload):
+        print("Confirmation")
+        url = "https://virtual-card-auth.herokuapp.com/visa_net/confirm/payment"
+        # url = "http://127.0.0.1:5001/visa_net/confirm/payment"
+        try:
+            r= request.put(url,json=payload,headers=headers, timeout=timeout)
+        except Exception as e:
+            print(e)
+            return e
+        response = jsonify(r.json())
+        response.status_code = r.status_code
+        return response
