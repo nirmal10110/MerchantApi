0e186f6578679675747453796e911f560bc7489d
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-29 06:37:37 +0530

    .

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from resource.merchant import (
 from resource.history import History
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://cdlvssvkcndjxs:063d2145766954c50cc0ec5a2d915903ec37a2a8d8c3a553dff53c75211c8208@ec2-34-194-198-176.compute-1.amazonaws.com:5432/d2t5dqf8i41ce4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["JWT_BLACKLIST_ENABLED"] = True  # enable blacklist feature
