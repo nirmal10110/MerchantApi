c15a56a0919d6a0b775a35b92cd486e129525fdb
nirmal10110 <nirmal.lather10110@gmail.com>
2020-06-29 13:18:37 +0530

    Shifted db to Postgres

--- authApi/visaNet.py
+++ authApi/visaNet.py
@@ -1,30 +1,31 @@
 import requests
 
-
 headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 timeout = 20
+
+INTERNAL_SERVER_ERROR = "Internal Server Error"
 
 
 class VisaNet:
     @classmethod
-    def AmountConfirmation(cls,payload):
+    def AmountConfirmation(cls, payload):
         print("AmountConfirmation")
         url = "https://virtual-card-auth.herokuapp.com/visa_net/payment"
         # url = "http://127.0.0.1:5001/visa_net/payment"
         try:
-            r= requests.post(url,json=payload,headers=headers, timeout=timeout)
+            r = requests.post(url, json=payload, headers=headers, timeout=timeout)
             return r
         except Exception as e:
             print(e)
             return {"msg": INTERNAL_SERVER_ERROR}, 500
-        
+
     @classmethod
     def TransactionConfirmation(cls, payload):
         print("Confirmation")
         url = "https://virtual-card-auth.herokuapp.com/visa_net/confirm/payment"
         # url = "http://127.0.0.1:5001/visa_net/confirm/payment"
         try:
-            r= request.put(url,json=payload,headers=headers, timeout=timeout)
+            r = request.put(url, json=payload, headers=headers, timeout=timeout)
         except Exception as e:
             print(e)
             return e
